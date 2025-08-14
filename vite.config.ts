4e17f1b118832fdb44aae0b942b3d40cbcb8b087
AaronIsserow <issaar001@myuct.ac.za>
2025-08-14 18:26:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/aaron-mechatronic-folio/',
   server: {
     host: "::",
     port: 8080,
